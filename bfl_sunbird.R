9038a35b3efa9c5d6812158a4c471b41f4212a69
Mark Dawson <mark.dawson@swansea.ac.uk>
2019-09-11 11:05:48 +0100

    changes from Josh

--- bfl_sunbird.R
+++ bfl_sunbird.R
@@ -6,19 +6,17 @@
 # 2) The we can use barriers to cut these rivers 
 # and calculate barrier free length (BFL)
 
-# Time difference of 1.809308 mins for Corsica only
+# Completes in 34 seconds for Corsica using 2 cores
 
 # usage: Rscript bfl_sunbird.R FR_rivers.shp FR_barriers.shp 2192 2
 
 library(dplyr)
 library(sf)
+library(parallel)
+library(lwgeom)
 
+# both versions of the st_parallel function
 source("st_parallel.R")
-
-START <- Sys.time()
-
-# what time is it?
-timedate <- paste0(format(Sys.time(), "%H%M%S"), "_", Sys.Date())
 
 # inputs
 args <- commandArgs(trailingOnly = TRUE)
@@ -28,100 +26,135 @@ barriers <- args[2] # format barriers.shp # snapped barriers because snapping ta
 epsg <- as.numeric(args[3]) # format 2192
 ncores <- as.numeric(args[4]) # format 1
 
-# barriers <- 'CO_barriers_ED50_snapped.shp'
-# basinRivers <- 'basinRivers143026_2019-07-04.shp'
-# epsg <- 2192
-# ncores <- 10
-
 message(paste0('Calculating barrier free length for ', basinRivers, ' using ', ncores, ' cores.'))
 
 # rivers
 basinRivers <- st_read(dsn = basinRivers) %>% st_transform(epsg)
 
 # barriers
-snapped <- st_read(dsn = barriers) %>%
+barriers <- st_read(dsn = barriers) %>%
   select(LabelAtlas) %>%
+  st_transform(epsg)
+
+snapped <- barriers
+
+# study site boundary buffered to 10 km
+boundary <- 'FR_boundary_10km.shp'
+boundary <- st_read(dsn = boundary) %>% 
   st_transform(epsg)
 
 ##############
 # Second, cut rivers with points
 ##############
 
-# # Snap barriers to river
-# st_snap_points = function(x, y, max_dist = 1000) {
-# 
-#   if (inherits(x, "sf")) n = nrow(x)
-#   if (inherits(x, "sfc")) n = length(x)
-# 
-#   out = do.call(c,
-#                 lapply(seq(n), function(i) {
-#                   nrst = st_nearest_points(st_geometry(x)[i], y)
-#                   nrst_len = st_length(nrst)
-#                   nrst_mn = which.min(nrst_len)
-#                   if (as.vector(nrst_len[nrst_mn]) > max_dist) return(st_geometry(x)[i])
-#                   return(st_cast(nrst[nrst_mn], "POINT")[2])
-#                 })
-#   )
-#   return(out)
-# }
-# 
-# snapped <- st_snap_points(barriers, basinRivers, 100) %>%
-#   st_cast("POINT") %>%
-#   st_zm(drop = TRUE, what = "ZM")
+# Functions
 
-message('Barriers snapped to rivers.')
+# Snap barriers to river
+st_snap_points_par = function(x, y, max_dist = 1000) {
+
+  if (inherits(x, "sf")) n = nrow(x)
+  if (inherits(x, "sfc")) n = length(x)
+
+  out = do.call(c,
+                mclapply(seq(n), function(i) {
+                  nrst = st_nearest_points(st_geometry(x)[i], y)
+                  nrst_len = st_length(nrst)
+                  nrst_mn = which.min(nrst_len)
+                  if (as.vector(nrst_len[nrst_mn]) > max_dist) return(st_geometry(x)[i])
+                  return(st_cast(nrst[nrst_mn], "POINT")[2])
+                })
+  )
+  return(out)
+}
+
+# A helper function that erases all of y from x: 
+st_erase = function(x, y) st_difference(x, st_union(st_union(y))) # using st_union instead of st_combine
+
+message('Buffering barriers for cutting the river...')
+buffBarriers <- st_parallel(snapped, st_buffer, ncores, dist = 0.0001) %>%
+  as('Spatial') %>%
+  st_as_sf() %>%
+  st_transform(epsg)
+
+# Snap barriers
+##############
+## snapping is quick in QGIS so skip this step and use QGIS
+## if your dataset is large
+##############
+message('Snapping barriers snapped to the river...') 
+# snapped <- st_snap_points_par(barriers, basinRivers, 100) %>%
+#   st_cast("POINT") %>%
+#   st_zm(drop = TRUE, what = "ZM")  %>%
+#   as('Spatial') %>%
+#   st_as_sf() %>%
+#   st_transform(epsg)
 
 # Buffer barriers
-buffBarriers <- st_parallel(snapped, st_buffer, ncores, dist = 0.0001)
-message('Barriers buffered for cutting river.')
-
-# st_write(bufferedBarriers, paste0("bufferedBarriers", timedate, ".shp"), delete_layer = TRUE) # overwrites
+message('Buffering barriers for cutting the river...')
+buffBarriers <- st_parallel(snapped, st_buffer, ncores, dist = 0.0001) %>%
+  as('Spatial') %>%
+  st_as_sf() %>%
+  st_transform(epsg)
 
 # Difference river network using buffered barriers
-# A helper function that erases all of y from x: 
-st_erase = function(x, y) st_difference(x, st_union(st_union(y))) # use st_union instead of st_combine
-diffRivers <- st_parallel(basinRivers, st_erase, ncores, buffBarriers) # can get stuck here with invalid geom
-message('Rivers erased using buffered barriers.')
-# st_write(differencedRivers, paste0("differencedRivers", timedate, ".shp"), delete_layer = TRUE) # overwrites
+message('Erasing rivers erased using buffered barriers...')
+diffRivers <- st_parallel(basinRivers, st_erase, ncores, buffBarriers) %>%
+  st_transform(epsg) # can get stuck here with invalid geom
 
 # Create river polygons and split them into seperate segments
+message('Buffering differenced rivers...')
 buffRivers <- st_parallel(diffRivers, st_buffer, ncores, dist = 0.00009)
-message('Rivers buffered.')
-dissRivers <- st_parallel(buffRivers, st_union, ncores)
-message('Rivers dissolved.')
-singlepartpoly <- st_cast(dissRivers, 'POLYGON') %>% 
-  as('Spatial') %>% 
-  st_as_sf() %>%
-  mutate(id = as.factor(row_number()))
-message('Rivers now singlepart polygons.')
 
-########
-# need to parallelise this bit
-########
-singlepartline <- do.call(rbind,lapply(1:nrow(diffRivers),function(i){st_cast(diffRivers[i,],"LINESTRING")}))
-message('Rivers now singlepart lines.')
+# Clip river polygons to study site boundary
+clipRiver <- st_parallel(buffRivers, st_crop, ncores, boundary)
+
+# Fix geom just in case
+message('Fixing invald geometry...')
+geomfix <- st_parallel(clipRiver, st_make_valid, ncores) %>% 
+    as('Spatial') %>% 
+    st_as_sf()
+
+# Dissolving buffered and clipped rivers
+message('Dissolving river polygons...')
+dissRivers <- st_parallel(geomfix, st_union, ncores) %>% 
+    as('Spatial') %>% 
+    st_as_sf()
+
+message('Casting river polygon into singlepart polygons...')
+singlepartpoly <- st_cast(dissRivers, 'POLYGON') %>%
+  mutate(id = as.factor(row_number()))
+
+# I think this is parallelised. Although it's only 0.5 seconds faster than in series...
+message('Converting river polygons into river lines in parallel...')
+singlepartline <- do.call(rbind, mclapply(1:nrow(diffRivers), 
+  function(i){st_cast(diffRivers[i,],"LINESTRING")}))
 
 # Bring attributes back together
+message('Joining attributes joined back to basin rivers...')
 joinedRivers <- st_parallel(singlepartline, st_join, ncores, singlepartpoly)
-message('Attributes joined back to basin rivers.')
 
 # calculate components of BFL
+message('Generating final output..')
 joinedRivers$seglen <- st_length(joinedRivers)
-basinlen <- joinedRivers %>% group_by(basinID) %>% summarise(basinlen = sum(seglen)) %>% st_drop_geometry()
-BFL <- joinedRivers %>% group_by(id) %>% summarise(BFL = sum(seglen)) %>% st_drop_geometry()
 
-# calculate BFLS
+basinlen <- joinedRivers %>% 
+    group_by(basinID) %>% 
+    summarise(basinlen = sum(seglen)) %>% 
+    st_drop_geometry()
+
+fraglen <- joinedRivers %>% 
+  group_by(id) %>%
+  summarise(fraglen = sum(seglen)) %>%
+  st_drop_geometry()
+
 BFLS <- joinedRivers %>% 
   left_join(basinlen, by = 'basinID') %>% 
   left_join(fraglen, by = 'id') %>% 
-  mutate(BFLS = as.numeric(BFL/basinlen) )
-
-message('BFLS finished!')
+  mutate(BFLS = as.numeric(fraglen/basinlen))
 
 # write output
+message('Writing output...')
+timedate <- paste0(format(Sys.time(), "%H%M%S"), "_", Sys.Date())
 st_write(BFLS, paste0("BFLS", timedate, ".shp"), delete_layer = TRUE) # overwrites
 
-# timing
-END <- Sys.time()
-message('Time taken:')
-END - START
+quit()
